c0257b2050ec655b148351edc1b72ca4aeca2e5b
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-04-30 19:10:34 -0300

    feat: create promotional banner to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,15 @@
 import { Header } from "@/app/_components/common/header"
 import { Search } from "@/app/_components/common/search"
 import { CategoryList } from "@/app/_components/common/category-list"
+import { PromoBanner } from "./_components/promo-banner"
 
 const Home = () => {
   return (
-    <main>
+    <main className="min-h-[100dvh] overflow-hidden">
       <Header />
       <Search />
       <CategoryList />
+      <PromoBanner />
     </main>
   )
 }
