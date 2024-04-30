ba29b42a1113ed197a62f503048f3c72555a49f2
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-04-30 16:37:45 -0300

    feat: add header, search and category list components to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,15 @@
+import { Header } from "@/app/_components/common/header"
+import { Search } from "@/app/_components/common/search"
+import { CategoryList } from "@/app/_components/common/category-list"
+
 const Home = () => {
-  return <main>Home</main>
+  return (
+    <main>
+      <Header />
+      <Search />
+      <CategoryList />
+    </main>
+  )
 }
 
 export default Home
