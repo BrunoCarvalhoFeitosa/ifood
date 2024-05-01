50db2a5d90058b5d1c20d75004d2e643c340a869
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-01 00:18:04 -0300

    feat: adding margin bottom to promotional banner

--- app/(home)/_components/promo-banner.tsx
+++ app/(home)/_components/promo-banner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export const PromoBannerPizza = () => {
   return (
-    <section className="relative w-full px-5 py-6">
+    <section className="relative mb-24 w-full px-5 py-6">
       <div className="w-full bg-primary md:h-[340px] lg:h-[320px] xl:h-[490px]">
         <div className="flex h-full w-full flex-col justify-between xl:flex-row">
           <div className="h-full w-full p-8">
@@ -34,14 +34,16 @@ export const PromoBannerPizza = () => {
                 src="/images/pizza-image.png"
                 width={620}
                 height={450}
-                className="hidden w-[350px] md:w-[450px] xl:block xl:w-2/4"
+                quality={100}
+                className="hidden w-[350px] brightness-[0.80] md:w-[450px] xl:block xl:w-2/4"
                 alt="Até 30% de desconto em pizzas"
               />
               <Image
                 src="/images/pizza-image.png"
                 width={620}
                 height={450}
-                className="w-[350px] md:w-[450px] xl:w-2/4"
+                quality={100}
+                className="w-[350px] brightness-[0.80] md:w-[450px] xl:w-2/4"
                 alt="Até 30% de desconto em pizzas"
               />
             </div>
