a9925a55a9a06bc1a26ddf765db81a663975e3e8
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-04 00:48:05 -0300

    feat: create pizza product list in home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,92 +6,121 @@ import { ProductList } from "@/app/_components/common/product-list"
 import { PromoBannerPizza } from "./_components/promo-banner"
 
 const HomePage = async () => {
-  const [categories, restaurants, brazilianFood, japaneseFood, fastFood] =
-    await Promise.all([
-      db.category.findMany({}),
+  const [
+    categories,
+    restaurants,
+    pizzas,
+    brazilianFood,
+    japaneseFood,
+    fastFood
+  ] = await Promise.all([
+    db.category.findMany({}),
 
-      db.restaurant.findMany({}),
+    db.restaurant.findMany({}),
 
-      db.product.findMany({
-        where: {
-          category: {
-            name: "Comida Brasileira"
-          }
-        },
-        include: {
-          restaurant: {
-            select: {
-              name: true,
-              imageUrl: true,
-              deliveryFee: true,
-              deliveryTimeMinutes: true
-            }
+    db.product.findMany({
+      where: {
+        category: {
+          name: "Pizzas diversas"
+        }
+      },
+      include: {
+        restaurant: {
+          select: {
+            name: true,
+            imageUrl: true,
+            deliveryFee: true,
+            deliveryTimeMinutes: true
           }
         }
-      }),
+      }
+    }),
 
-      db.product.findMany({
-        where: {
-          category: {
-            name: "Comida Japonesa"
-          }
-        },
-        include: {
-          restaurant: {
-            select: {
-              id: true,
-              name: true,
-              imageUrl: true,
-              deliveryFee: true,
-              deliveryTimeMinutes: true,
-              categories: true,
-              products: true
-            }
+    db.product.findMany({
+      where: {
+        category: {
+          name: "Comida Brasileira"
+        }
+      },
+      include: {
+        restaurant: {
+          select: {
+            name: true,
+            imageUrl: true,
+            deliveryFee: true,
+            deliveryTimeMinutes: true
           }
         }
-      }),
+      }
+    }),
 
-      db.product.findMany({
-        where: {
-          category: {
-            name: "Hambúrgueres"
-          }
-        },
-        include: {
-          restaurant: {
-            select: {
-              id: true,
-              name: true,
-              imageUrl: true,
-              deliveryFee: true,
-              deliveryTimeMinutes: true,
-              categories: true,
-              products: true
-            }
+    db.product.findMany({
+      where: {
+        category: {
+          name: "Comida Japonesa"
+        }
+      },
+      include: {
+        restaurant: {
+          select: {
+            id: true,
+            name: true,
+            imageUrl: true,
+            deliveryFee: true,
+            deliveryTimeMinutes: true,
+            categories: true,
+            products: true
           }
         }
-      })
-    ])
+      }
+    }),
+
+    db.product.findMany({
+      where: {
+        category: {
+          name: "Hambúrgueres"
+        }
+      },
+      include: {
+        restaurant: {
+          select: {
+            id: true,
+            name: true,
+            imageUrl: true,
+            deliveryFee: true,
+            deliveryTimeMinutes: true,
+            categories: true,
+            products: true
+          }
+        }
+      }
+    })
+  ])
 
   return (
-    <main className="min-h-[100dvh] w-full overflow-hidden pb-24">
+    <div>
       <Header categories={categories} restaurants={restaurants} />
-      <Search />
-      <CategoryList />
-      <PromoBannerPizza />
-      <ProductList
-        title="O melhor da culinária brasileira"
-        products={brazilianFood}
-      />
-      <ProductList
-        title="O melhor da culinária japonesa"
-        products={japaneseFood}
-      />
-      <ProductList
-        title="Desconto ativo por tempo limitado"
-        products={fastFood}
-      />
-    </main>
+      <main className="min-h-[100dvh] w-full overflow-hidden">
+        <Search />
+        <CategoryList />
+        <PromoBannerPizza />
+        <div className="mt-8 flex flex-col gap-14">
+          <ProductList title="Pizzas" products={pizzas} />
+          <ProductList
+            title="O melhor da culinária brasileira"
+            products={brazilianFood}
+          />
+          <ProductList
+            title="O melhor da culinária japonesa"
+            products={japaneseFood}
+          />
+          <ProductList
+            title="Desconto ativo por tempo limitado"
+            products={fastFood}
+          />
+        </div>
+      </main>
+    </div>
   )
 }
 
