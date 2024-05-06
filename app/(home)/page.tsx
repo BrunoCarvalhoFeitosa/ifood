4d63f14d2b8fb686154339b83de56e6a0407eb95
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-05 23:58:00 -0300

    feat: remove pizzas shelf from home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,106 +6,83 @@ import { ProductList } from "@/app/_components/common/product-list"
 import { PromoBannerPizza } from "./_components/promo-banner"
 
 const HomePage = async () => {
-  const [
-    categories,
-    restaurants,
-    pizzas,
-    brazilianFood,
-    japaneseFood,
-    fastFood
-  ] = await Promise.all([
-    db.category.findMany({}),
+  const [categories, restaurants, brazilianFood, japaneseFood, fastFood] =
+    await Promise.all([
+      db.category.findMany({}),
 
-    db.restaurant.findMany({}),
+      db.restaurant.findMany({}),
 
-    db.product.findMany({
-      where: {
-        category: {
-          name: "Pizzas diversas"
-        }
-      },
-      include: {
-        restaurant: {
-          select: {
-            name: true,
-            imageUrl: true,
-            deliveryFee: true,
-            deliveryTimeMinutes: true
+      db.product.findMany({
+        where: {
+          category: {
+            name: "Comida Brasileira"
+          }
+        },
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+              imageUrl: true,
+              deliveryFee: true,
+              deliveryTimeMinutes: true
+            }
           }
         }
-      }
-    }),
+      }),
 
-    db.product.findMany({
-      where: {
-        category: {
-          name: "Comida Brasileira"
-        }
-      },
-      include: {
-        restaurant: {
-          select: {
-            name: true,
-            imageUrl: true,
-            deliveryFee: true,
-            deliveryTimeMinutes: true
+      db.product.findMany({
+        where: {
+          category: {
+            name: "Comida Japonesa"
+          }
+        },
+        include: {
+          restaurant: {
+            select: {
+              id: true,
+              name: true,
+              imageUrl: true,
+              deliveryFee: true,
+              deliveryTimeMinutes: true,
+              categories: true,
+              products: true
+            }
           }
         }
-      }
-    }),
+      }),
 
-    db.product.findMany({
-      where: {
-        category: {
-          name: "Comida Japonesa"
-        }
-      },
-      include: {
-        restaurant: {
-          select: {
-            id: true,
-            name: true,
-            imageUrl: true,
-            deliveryFee: true,
-            deliveryTimeMinutes: true,
-            categories: true,
-            products: true
+      db.product.findMany({
+        where: {
+          category: {
+            name: "Hambúrgueres"
+          }
+        },
+        include: {
+          restaurant: {
+            select: {
+              id: true,
+              name: true,
+              imageUrl: true,
+              deliveryFee: true,
+              deliveryTimeMinutes: true,
+              categories: true,
+              products: true
+            }
           }
         }
-      }
-    }),
-
-    db.product.findMany({
-      where: {
-        category: {
-          name: "Hambúrgueres"
-        }
-      },
-      include: {
-        restaurant: {
-          select: {
-            id: true,
-            name: true,
-            imageUrl: true,
-            deliveryFee: true,
-            deliveryTimeMinutes: true,
-            categories: true,
-            products: true
-          }
-        }
-      }
-    })
-  ])
+      })
+    ])
 
   return (
     <div>
       <Header categories={categories} restaurants={restaurants} />
       <main className="min-h-[100dvh] w-full overflow-hidden">
-        <Search />
+        <div className="px-5 py-6">
+          <Search />
+        </div>
         <CategoryList />
         <PromoBannerPizza />
         <div className="mt-8 flex flex-col gap-14">
-          <ProductList title="Pizzas" products={pizzas} />
           <ProductList
             title="O melhor da culinária brasileira"
             products={brazilianFood}
