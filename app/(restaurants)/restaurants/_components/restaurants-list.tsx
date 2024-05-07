d07f3efddad67b2bb0e8a7d87735cdff84726cb4
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-07 11:12:26 -0300

    fix: build error in search component

--- app/(restaurants)/restaurants/_components/restaurants-list.tsx
+++ app/(restaurants)/restaurants/_components/restaurants-list.tsx
@@ -1,25 +1,14 @@
 "use client"
-import { useEffect, useState, Suspense } from "react"
+import { useEffect, useState } from "react"
 import { Restaurant } from "@prisma/client"
 import { useSearchParams } from "next/navigation"
 import { searchRestaurants } from "@/app/_actions/search"
 import { RestaurantListItem } from "@/app/_components/common/restaurant/restaurant-list-item"
 import { Search } from "@/app/_components/common/search/search"
 
-const SuspenseBoundary = ({ children }: { children: React.ReactNode }) => (
-  <Suspense
-    fallback={
-      <div className="flex h-full w-full items-center justify-center">
-        Carregando...
-      </div>
-    }
-  >
-    {children}
-  </Suspense>
-)
-
 export const RestaurantsList = () => {
   const [restaurantsList, setRestaurantsList] = useState<Restaurant[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
   const params = useSearchParams()
   const term = params.get("search")
 
@@ -29,6 +18,7 @@ export const RestaurantsList = () => {
 
       const foundedRestaurants = await searchRestaurants(term)
       setRestaurantsList(foundedRestaurants)
+      setLoading(false)
     }
 
     getRestaurants()
@@ -38,35 +28,41 @@ export const RestaurantsList = () => {
     return null
   }
 
+  if (loading) {
+    return (
+      <div className="flex h-full w-full items-center justify-center">
+        Carregando...
+      </div>
+    )
+  }
+
   return (
-    <SuspenseBoundary>
-      <section className="w-full px-5 py-6">
-        <Search defaultValues={{ term }} />
-        <div className="mt-14">
-          <h1 className="text-xl font-extrabold lg:text-2xl">
-            {restaurantsList.length === 1 ? (
-              <>
-                <span className="mr-1">
-                  ({restaurantsList.length}) resultado encontrado para
-                </span>
-                <strong className="text-red-500">&quot;{term}&quot;</strong>
-              </>
-            ) : (
-              <>
-                <span className="mr-1">
-                  ({restaurantsList.length}) resultados encontrados para
-                </span>
-                <strong className="text-red-500">&quot;{term}&quot;</strong>
-              </>
-            )}
-          </h1>
-        </div>
-        <div className="mt-5 flex flex-col flex-wrap items-center gap-4 xl:flex-row">
-          {restaurantsList.map((restaurant) => (
-            <RestaurantListItem key={restaurant.id} restaurant={restaurant} />
-          ))}
-        </div>
-      </section>
-    </SuspenseBoundary>
+    <section className="w-full px-5 py-6">
+      <Search defaultValues={{ term }} />
+      <div className="mt-14">
+        <h1 className="text-xl font-extrabold lg:text-2xl">
+          {restaurantsList.length === 1 ? (
+            <>
+              <span className="mr-1">
+                ({restaurantsList.length}) resultado encontrado para
+              </span>
+              <strong className="text-red-500">&quot;{term}&quot;</strong>
+            </>
+          ) : (
+            <>
+              <span className="mr-1">
+                ({restaurantsList.length}) resultados encontrados para
+              </span>
+              <strong className="text-red-500">&quot;{term}&quot;</strong>
+            </>
+          )}
+        </h1>
+      </div>
+      <div className="mt-5 flex flex-col flex-wrap items-center gap-4 xl:flex-row">
+        {restaurantsList.map((restaurant) => (
+          <RestaurantListItem key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </section>
   )
 }
