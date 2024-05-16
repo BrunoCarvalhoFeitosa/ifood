e02778818e6a8ab55926499bfc125a756fe83b11
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-16 13:19:01 -0300

    feat: uopdate width and height of product images

--- app/_components/common/product/product-list-item-image.tsx
+++ app/_components/common/product/product-list-item-image.tsx
@@ -11,14 +11,14 @@ export const ProductListItemImage = ({
   product
 }: ProductListItemImageProps) => {
   return (
-    <div className="relative w-[210px] overflow-hidden md:w-[210px] lg:w-[340px]">
-      <div className="h-[150px] w-full lg:h-[240px]">
+    <div className="relative w-[210px] overflow-hidden md:w-[210px] lg:w-[calc((100dvw/4)-25px)]">
+      <div className="bg-gray-210 h-[150px] w-full lg:h-[300px]">
         <Image
           fill
           src={product.imageUrl}
           alt={product.name}
           quality={100}
-          className="cursor-zoom-in object-cover transition-transform duration-300 hover:scale-110"
+          className="cursor-zoom-in object-cover object-center transition-transform duration-300 hover:scale-110"
         />
       </div>
       <div className="absolute left-3 top-3 z-10">
--- app/_components/common/product/product-list.tsx
+++ app/_components/common/product/product-list.tsx
@@ -1,7 +1,5 @@
 import { Prisma } from "@prisma/client"
-import { Button } from "@/app/_components/ui/button"
 import { ProductListItem } from "./product-list-item"
-import { ChevronRightIcon } from "lucide-react"
 
 interface ProductListProps {
   title: string
@@ -24,23 +22,8 @@ export const ProductList = ({ title, products }: ProductListProps) => {
     <section className="w-full px-5">
       <div className="flex flex-col justify-between md:flex-row md:items-center">
         <div>
-          <h2 className="text-2xl font-extrabold md:text-xl">{title}</h2>
+          <h2 className="text-lg font-extrabold md:text-xl">{title}</h2>
         </div>
-        {products.length > 6 && (
-          <div>
-            <Button
-              type="button"
-              variant="ghost"
-              size="lg"
-              className="rounded-full p-0 text-lg font-extrabold text-primary hover:text-primary md:text-base"
-            >
-              <span>Ver todos</span>
-              <span>
-                <ChevronRightIcon />
-              </span>
-            </Button>
-          </div>
-        )}
       </div>
       <div className="custom-scrollbar flex items-center gap-5 overflow-x-auto py-3">
         {products.slice(0, 12).map((product) => (
