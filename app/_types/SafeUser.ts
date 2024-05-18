eef80c0d6a777d36c075b0e71480dc98535e1433
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-18 00:04:38 -0300

    fix: build error

--- app/_types/SafeUser.ts
+++ app/_types/SafeUser.ts
@@ -2,9 +2,10 @@ import { User } from "@prisma/client"
 
 export type SafeUser = Omit<
   User,
-  "createdAt" | "updatedAt" | "emailVerified"
+  "createdAt" | "updatedAt" | "emailVerified" | "hashedPassword"
 > & {
   createdAt: string
   updatedAt: string
   emailVerified: string | null
+  hashedPassword: string
 }
