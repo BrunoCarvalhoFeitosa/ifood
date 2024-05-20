7dbb8debb6539daf14ff74757945ef7b1aeb4e21
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-19 21:34:20 -0300

    feat: create not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,30 @@
+import db from "@/app/_libs/prisma"
+import { Header } from "./_components/common/header"
+import { AccountIllustration } from "@/public/svgs/account-illustration"
+
+const NotFoundPage = async () => {
+  const [categories, restaurants] = await Promise.all([
+    db.category.findMany({}),
+
+    db.restaurant.findMany({})
+  ])
+
+  return (
+    <div>
+      <Header categories={categories} restaurants={restaurants} />
+      <main>
+        <div className="flex h-[calc(100dvh-100px)] w-full flex-col items-center justify-center gap-4 px-5">
+          <div className="mx-auto w-2/3 text-center">
+            <h1 className="text-6xl font-extrabold">404</h1>
+            <p>Ooops, a página que você tentou acessar não existe.</p>
+          </div>
+          <div className="mx-auto w-full xl:w-2/5">
+            <AccountIllustration />
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default NotFoundPage
