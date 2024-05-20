469b5f44a1c8402703ddc78711b43f2a76f264ee
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-19 21:51:21 -0300

    feat: add sticky position to header

--- app/_components/common/header/index.tsx
+++ app/_components/common/header/index.tsx
@@ -1,3 +1,4 @@
+"use server"
 import getCurrentUser from "@/app/_actions/getCurrentUser"
 import { Category, Restaurant } from "@prisma/client"
 import { HeaderLogo } from "./header-logo"
@@ -13,7 +14,7 @@ export const Header = async ({ categories, restaurants }: HeaderProps) => {
   const currentUser = await getCurrentUser()
 
   return (
-    <header className="flex w-full items-center justify-between p-5">
+    <header className="sticky top-0 z-50 flex w-full items-center justify-between bg-white p-5">
       <HeaderLogo />
       <div className="flex items-center gap-1">
         <HeaderCart />
