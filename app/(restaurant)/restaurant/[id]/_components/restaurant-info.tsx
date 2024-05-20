f813eee74b9c0d45aa2467f16861db23029cfcd1
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-19 21:39:54 -0300

    feat: add slide button context to mobile restaurant page

--- app/(restaurant)/restaurant/[id]/_components/restaurant-info.tsx
+++ app/(restaurant)/restaurant/[id]/_components/restaurant-info.tsx
@@ -1,8 +1,11 @@
 "use client"
 import { Prisma } from "@prisma/client"
-import { RestaurantDelivery } from "./restaurant-delivery"
+import { cn } from "@/app/_libs/utils"
+import { useSlideButton } from "@/app/_contexts/SlideButtonContext"
+import { RestaurantSlideButton } from "./restaurant-slide-button"
 import { RestaurantResume } from "./restaurant-resume"
 import { RestaurantCategories } from "./restaurant-categories"
+import { RestaurantDelivery } from "./restaurant-delivery"
 
 interface RestaurantInfoProps {
   restaurant: Prisma.RestaurantGetPayload<{
@@ -13,8 +16,16 @@ interface RestaurantInfoProps {
 }
 
 export const RestaurantInfo = ({ restaurant }: RestaurantInfoProps) => {
+  const { isOpen } = useSlideButton()
+
   return (
-    <div className="relative z-20 -mt-10 flex flex-1 flex-col rounded-tl-[40px] rounded-tr-[40px] bg-white px-5 py-6 transition-all duration-500 md:-mt-14 md:rounded-tl-[60px] md:rounded-tr-[60px] xl:mt-0 xl:rounded-none xl:py-0">
+    <div
+      className={cn(
+        "relative z-20 -mt-10 flex flex-1 flex-col rounded-tl-[40px] rounded-tr-[40px] bg-white px-5 py-6 transition-all duration-500 md:-mt-14 md:rounded-tl-[60px] md:rounded-tr-[60px] xl:mt-0 xl:rounded-none xl:py-0",
+        `${isOpen ? "-mt-48 md:-mt-72 lg:-mt-96" : "-mt-10 xl:mt-0"}`
+      )}
+    >
+      <RestaurantSlideButton />
       <RestaurantResume restaurant={restaurant} />
       <RestaurantCategories restaurant={restaurant} />
       <RestaurantDelivery restaurant={restaurant} />
---
+++ app/(restaurant)/restaurant/[id]/_components/restaurant-slide-button.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useSlideButton } from "@/app/_contexts/SlideButtonContext"
+import { Button } from "@/app/_components/ui/button"
+
+export const RestaurantSlideButton = () => {
+  const { setIsOpen, isOpen } = useSlideButton()
+
+  return (
+    <div className="mb-5 flex justify-center xl:hidden">
+      <Button
+        type="button"
+        variant="ghost"
+        className="h-2 w-20 rounded-full bg-zinc-300 p-0 hover:bg-zinc-300"
+        onClick={() => setIsOpen(!isOpen)}
+      />
+    </div>
+  )
+}
