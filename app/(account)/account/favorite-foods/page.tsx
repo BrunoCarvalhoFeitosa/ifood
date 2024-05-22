283df1cd2151510cb7dc69d7c525474cf5cf07f7
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-05-22 20:04:45 -0300

    feat: create account favorite foods page

--- app/(account)/account/favorite-foods/page.tsx
+++ app/(account)/account/favorite-foods/page.tsx
@@ -1,7 +1,48 @@
 "use server"
+import db from "@/app/_libs/prisma"
+import getCurrentUser from "@/app/_actions/getCurrentUser"
+import { ProductListItem } from "@/app/_components/common/product/product-list-item"
 
-const AccountFavoriteFoodsPage = () => {
-  return <div>Favorite foods</div>
+const AccountFavoriteFoodsPage = async () => {
+  const currentUser = await getCurrentUser()
+
+  const userFavoriteProducts = await db.userFavoriteProduct.findMany({
+    where: {
+      userId: currentUser?.id
+    },
+    include: {
+      product: {
+        include: {
+          restaurant: true,
+          category: true,
+          favoritedByUsers: true
+        }
+      }
+    }
+  })
+
+  return (
+    <div>
+      {userFavoriteProducts.length >= 1 ? (
+        <div className="custom-scrollbar flex items-center gap-14 overflow-x-auto py-3 lg:gap-5">
+          {userFavoriteProducts.map(({ product }) => (
+            <ProductListItem
+              key={product.id}
+              product={product}
+              currentUser={currentUser}
+              userFavoriteProducts={userFavoriteProducts}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h3 className="font-semibold">
+            Nenhuma comida foi marcada como favorita.
+          </h3>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default AccountFavoriteFoodsPage
