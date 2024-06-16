73473ccbd0dfa9c292985893d7a89c9c880e671e
BrunoCarvalhoFeitosa <brunocarvalhofeitosa@outlook.com>
2024-06-16 15:16:32 -0300

    feat: increase font size to handle login header text

--- app/_components/common/header/header-unautheticated-content.tsx
+++ app/_components/common/header/header-unautheticated-content.tsx
@@ -6,9 +6,7 @@ import { LogInIcon } from "lucide-react"
 export const HeaderUnauthenticatedContent = () => {
   return (
     <div className="mt-8 flex items-center justify-between gap-3 border-b border-solid pb-3">
-      <div className="text-sm font-semibold xl:text-base">
-        Olá. Faça seu login!
-      </div>
+      <div className="text-lg font-semibold">Olá. Faça seu login!</div>
       <Link href="/sign-in" className="w-fit">
         <Button type="button" variant="default" size="icon">
           <LogInIcon size={20} />
